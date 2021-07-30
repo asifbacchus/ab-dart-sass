1972e6c5cf9c4e0efb71befa2608d49f65b48001
Asif Bacchus <asif@asifbacchus.dev>
2021-07-29 22:19:19 -0600

    feature(entrypoint): add oneshot option

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@
 
 if [ "$1" = "shell" ]; then
     exec /bin/bash
+elif [ "$1" = "oneshot" ]; then
+    exec /opt/dart-sass/sass -s "$SASS_STYLE" --update --stop-on-error /sass:/css
 elif [ -n "$1" ]; then
     exec "$@"
 else
